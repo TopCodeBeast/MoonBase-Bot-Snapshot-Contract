c54d65b0497117395b7a5ef5bb4c82e82cf3474a
Django（张弓） <gongzhang67@gmail.com>
2022-10-05 21:05:46 +1100

    bug repair

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-set -e
 
 RUSTFLAGS='-C link-arg=-s' cargo build --target wasm32-unknown-unknown --release
 cp target/wasm32-unknown-unknown/release/nepbot_snapshot.wasm ./res/
--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-near dev-deploy res/nepbot_snapshot.wasm 
+near dev-deploy res/nepbot_snapshot.wasm --initFunction new --initArgs '{"public_key":"snapshot1.0xjacktest1.testnet"}'
